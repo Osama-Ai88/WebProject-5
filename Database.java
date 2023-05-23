cd788bf736f459b0233811a3a744a9004490431f
Osama Alessa <2019901021@stu.yu.edu.jo>
2023-05-24 02:40:01 +0300

    This v2.0

--- Database.java
+++ Database.java
@@ -2,6 +2,8 @@ public class Database {
 
     public static void main(String[] args)
     {
+
         System.out.println("This is Database");
+        System.out.println("This is Database 2");
     }
 }
